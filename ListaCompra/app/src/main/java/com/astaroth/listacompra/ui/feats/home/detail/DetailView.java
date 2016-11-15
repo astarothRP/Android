78df7b66844eac26dcac67337ce9a8d2c55a2792
Ricardo Pérez <ricardo.perez@intelygenz.com>
2016-11-15 15:40:14 +0100

    Animation when click fab button

--- ListaCompra/app/src/main/java/com/astaroth/listacompra/ui/feats/home/detail/DetailView.java
+++ ListaCompra/app/src/main/java/com/astaroth/listacompra/ui/feats/home/detail/DetailView.java
@@ -1,9 +1,15 @@
 package com.astaroth.listacompra.ui.feats.home.detail;
 
+import android.animation.Animator;
+import android.annotation.TargetApi;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.ContentLoadingProgressBar;
 import android.support.v7.widget.RecyclerView;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import com.astaroth.listacompra.R;
 import com.astaroth.listacompra.domains.Collection;
 import com.astaroth.listacompra.domains.Detail;
@@ -18,6 +24,7 @@ class DetailView extends BaseFragmentView {
 	private DetailRenderer renderer;
 	private FloatingActionButton addDetailFab;
 	private DetailRenderer.DetailRendererListener rendererListener = getDetailRendererListener();
+	private View coordinatorLayout;
 
 	DetailView(ViewListener listener) {
 		super(R.layout.detail_fragment_layout);
@@ -26,7 +33,7 @@ class DetailView extends BaseFragmentView {
 
 	@Override
 	protected void setUpView(View view) {
-		setUpFabClick((FloatingActionButton)view.findViewById(R.id.detail_fab));
+		setUpFabClick((FloatingActionButton) view.findViewById(R.id.detail_fab), view.findViewById(R.id.detail_layout));
 		renderer = new DetailRenderer((RecyclerView) view.findViewById(R.id.detail_recycler),
 									  view.findViewById(R.id.detail_nocontent_textview),
 									  (ContentLoadingProgressBar) view.findViewById(
@@ -34,7 +41,8 @@ class DetailView extends BaseFragmentView {
 
 	}
 
-	private void setUpFabClick(FloatingActionButton fabButton) {
+	private void setUpFabClick(FloatingActionButton fabButton, View rootView) {
+		this.coordinatorLayout = rootView;
 		addDetailFab = fabButton;
 		addDetailFab.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -42,6 +50,30 @@ class DetailView extends BaseFragmentView {
 				showDetailDialog(null);
 			}
 		});
+		addDetailFab.setOnTouchListener(new View.OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (android.os.Build.VERSION.SDK_INT >= 21) {
+					animateRevealColorFromCoordinates(coordinatorLayout, (int) event.getRawX(), (int) event.getRawY());
+				}
+				return false;
+			}
+		});
+	}
+
+	@TargetApi(21)
+	private Animator animateRevealColorFromCoordinates(View viewRoot, int x, int y) {
+		float finalRadius = (float) Math.hypot(viewRoot.getWidth(), viewRoot.getHeight());
+		int color = ContextCompat.getColor(viewRoot.getContext(), R.color.grey_medium);
+		int duration = viewRoot.getResources()
+			.getInteger(R.integer.anim_duration);
+
+		Animator anim = ViewAnimationUtils.createCircularReveal(viewRoot, x, y, 0, finalRadius);
+		viewRoot.setBackgroundColor(color);
+		anim.setDuration(duration);
+		anim.setInterpolator(new AccelerateDecelerateInterpolator());
+		anim.start();
+		return anim;
 	}
 
 	private void showDetailDialog(Detail detail) {
@@ -55,6 +87,17 @@ class DetailView extends BaseFragmentView {
 											@Override
 											public void delete(Detail detail) {
 												listener.deleteDetail(detail);
+											}
+
+											@Override
+											public void onDismis() {
+												setDefaultBackgroundColor();
+											}
+
+											private void setDefaultBackgroundColor() {
+												int color = ContextCompat.getColor(coordinatorLayout.getContext(), R.color
+													.grey_light);
+												coordinatorLayout.setBackgroundColor(color);
 											}
 										});
 	}
